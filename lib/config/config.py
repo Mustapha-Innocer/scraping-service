078715e86b07ba1330e3e7cbc13f52ef41509a10
Mustapha Innocer <mustaphainnocer@gmail.com>
2025-03-22 11:44:52 +0000

    add cache TTLs to config

--- lib/config/config.py
+++ lib/config/config.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +9,6 @@ KAFKA_PORT = os.getenv("KAFKA_PORT")
 
 REDIS_SERVER = os.getenv("REDIS_SERVER")
 REDIS_PORT = os.getenv("REDIS_PORT")
+
+TTL_NEW_TAG = os.getenv("TTL_NEW_TAG")
+TTL_ERRORED_TAG = os.getenv("TTL_ERRORED_TAG")

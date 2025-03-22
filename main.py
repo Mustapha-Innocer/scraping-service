eb6efe9d18ef3198eddc6b7fd36439e7686b06b8
Mustapha Innocer <mustaphainnocer@gmail.com>
2025-03-22 15:44:23 +0000

    add a heartbeat function

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ import asyncio
 from lib.logging.logger import LOGGER
 from lib.sources.ScrapingSources.CityNews.CityNews import CityNews
 from lib.sources.ScrapingSources.TechCrunch.TechCrunch import TechCrunch
+
 # from lib.sources.ScrapingSources.Yahoo.Yahoo import Yahoo
 
 
+async def heart_beat():
+    while True:
+        await asyncio.sleep(5)
+        LOGGER.info("Heart Beat Received ...")
+
+
 async def main():
-    cn = CityNews()
-    tc = TechCrunch()
+    cn = CityNews(60)
+    tc = TechCrunch(30)
     news_sources = [
         cn,
         tc,
     ]
-    await asyncio.gather(*[source.push_data() for source in news_sources])
+    await asyncio.gather(
+        *[source.push_data() for source in news_sources], heart_beat()
+    )
 
 
 if __name__ == "__main__":
@@ -21,6 +30,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        LOGGER.info(
-            f"{'*' * 10} Scraping Service closed {'*' * 10}"
-        )
+        LOGGER.info(f"{'*' * 10} Scraping Service closed {'*' * 10}")
